b9f766bfb3bfd1be7cb361a201989bf10c50a82a
nekzabirov <n.zabirov@1638.tech>
2025-10-30 00:26:06 +0200

    Refactor CMake configuration to simplify `libpqxx` dependency handling for non-Apple UNIX systems. Removed redundant paths and streamlined setup by relying on modern CMake targets.

--- cmake/dependency/pqxx.dependency.cmake
+++ cmake/dependency/pqxx.dependency.cmake
@@ -3,15 +3,6 @@ if (APPLE)
     find_package(libpqxx CONFIG REQUIRED)
     set(PQXX_LIBS libpqxx::pqxx)
     set(PQXX_LIBS_INCLUDE_DIRS "") # Не нужно, libpqxx::pqxx уже содержит пути
-elseif (UNIX AND NOT APPLE)
-    find_package(PostgreSQL REQUIRED)
-
-    set(LIBPQXX_TARGET       "${CMAKE_CURRENT_SOURCE_DIR}/libs/libpqxx/lib/amd64/libpqxx-7.10.a")
-    set(LIBPQXX_INCLUDE_DIRS "${CMAKE_CURRENT_SOURCE_DIR}/libs/libpqxx/include")
-
-    set(PQ_LIB ${PostgreSQL_LIBRARIES})
-    set(PQXX_LIBS ${LIBPQXX_TARGET} ${PQ_LIB})
-    set(PQXX_LIBS_INCLUDE_DIRS ${LIBPQXX_INCLUDE_DIRS})
 else()
     find_package(libpqxx CONFIG REQUIRED)
     set(PQXX_LIBS libpqxx::pqxx)
